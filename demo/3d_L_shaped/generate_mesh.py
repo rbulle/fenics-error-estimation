30ce823b7df128b23b9a91382143950072f858ff
rbulle <bulleraph@free.fr>
2020-01-16 11:09:08 +0100

    3D L-shaped mesh created

--- demo/3d_L_shaped/generate_mesh.py
+++ demo/3d_L_shaped/generate_mesh.py
@@ -2,7 +2,7 @@ import pygmsh as pg
 import meshio
 
 lc=0.1
-geom = pg.opencascard.Geometry()
+geom = pg.opencascade.Geometry()
 
 big_box = geom.add_box([-0.5, -0.5, 0.], [1., 1., 0.5], char_length = lc)
 corner_box = geom.add_box([-0.5, -0.5, 0.], [0.5, 0.5, 0.5], char_length = lc)
