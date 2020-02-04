4a5ae1966d4fec32e6e279bcd41b483cbbd73558
rbulle <bulleraph@free.fr>
2020-02-04 11:27:20 +0100

    Added homogeneous DBC 3D L-shaped test case

---
+++ demo/3d_homog_dirichlet/generate_mesh.py
@@ -0,0 +1,16 @@
+import pygmsh as pg
+import meshio
+
+lc=0.1
+geom = pg.opencascade.Geometry()
+
+big_box = geom.add_box([-0.5, -0.5, 0.25], [1., 1., -0.5], char_length = lc)
+corner_box = geom.add_box([-0.5, -0.5, 0.25], [0.5, 0.5, -0.5], char_length = lc)
+
+geom.boolean_difference([big_box], [corner_box])
+
+mesh = pg.generate_mesh(geom)
+
+meshio.write("mesh.xdmf", meshio.Mesh(
+	points = mesh.points,
+	cells={"tetra": mesh.cells["tetra"]}))
