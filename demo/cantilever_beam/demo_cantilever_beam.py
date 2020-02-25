a9eaaa1c64abb62ee5e4f06a274be47c2910e72c
rbulle <bulleraph@free.fr>
2020-02-25 11:22:22 +0100

    Added cantilever beam demo. Not working yet.

--- demo/cantilever_beam/demo_cantilever_beam.py
+++ demo/cantilever_beam/demo_cantilever_beam.py
@@ -32,6 +32,17 @@ parameters["ghost_mode"] = "shared_facet"
 parameters["form_compiler"]["optimize"] = True
 parameters["form_compiler"]["cpp_optimize"] = True
 
+coef_load = -(3./2.)*5.**(-2)*1.e9
+E = 210.e9      # Young's modulus
+nu = 0.4999     # Poisson's ratio
+kappa = E*nu/((1. + nu)*(1.-2.*nu))
+Eb = E/(1.-nu**2)
+nub = nu/(1.-nu**2)
+mu = E/(2.*(1.-nu))
+lmbda = E*nu/((1.+nu)*(1.-2.*nu))
+Le = 200.e-3     # Length of the beam
+D = 100.e-3     # height of the beam
+I = E*D**3/12.  # Second-area moment
 
 # k_f\k_g for bw definition
 k_f = 3
@@ -40,24 +51,24 @@ k_g = 1
 path = 'bw_P{}_P{}/'.format(k_f, k_g)
 
 def main():
-    K = 4
-    mesh = UnitSquareMesh(K, K)
-    mesh.coordinates()[:] -= 0.5
-    mesh.coordinates()[:] *= 2.
-    
+    K = 2
+    mesh = UnitSquareMesh(2*K, K)
+
+    mesh.coordinates()[:, 0] *= 2.
+    mesh.coordinates()[:, 1] -= 0.5
+
     X_el = VectorElement('CG', triangle, 2)
     M_el = FiniteElement('CG', triangle, 1)
-    L_el = FiniteElement('R', triangle, 0)
 
-    V_el = MixedElement([X_el, M_el, L_el])
+    V_el = MixedElement([X_el, M_el])
 
     results = []
-    for i in range(0, 5):
+    for i in range(0, 4):
         V = FunctionSpace(mesh, V_el)
-        
-        result = {}
-        result['num_cells'] = V.mesh().num_cells()
 
+        result = {}
+        w_h, exact_err = solve(V)
+        print('V dim = {}'.format(V.dim()))
         w_h, err = solve(V)
         print('Exact error = {}'.format(err))
         result['exact_error'] = err
@@ -74,15 +85,14 @@ def main():
         eta_res = residual_estimate(w_h)
         result['error_res'] = np.sqrt(eta_res.vector().sum())
         print('Res = {}'.format(np.sqrt(eta_res.vector().sum())))
+
         '''
         print('Marking...')
         markers = fenics_error_estimation.dorfler(eta_h, 0.5)
         print('Refining...')
         mesh = refine(mesh, markers, redistribute=True)
         '''
-
         mesh = refine(mesh)
-
         with XDMFFile('output/{}bank-weiser/mesh_{}.xdmf'.format(path, str(i).zfill(4))) as f:
             f.write(mesh)
 
@@ -104,33 +114,39 @@ def main():
 
 
 def solve(V):
-    """Solve Stokes problem for viscous incompressible flow using a P2-P1 mixed finite
+    """Solve nearly-incompressible elasticity using a P2-P1 mixed finite
     element method. This is completely standard."""
     mesh = V.mesh()
 
     f = Constant((0., 0.))
 
-    w_exact = Expression(('20.*x[0]*pow(x[1], 3)', '5.*pow(x[0], 4)-5.*pow(x[1], 4)', '60.*pow(x[0], 2)*x[1]- 20.*pow(x[1], 3)'), degree = 4)
+    u_exact = Expression(('- ((coef_load*pow(x[1], 2))*x[1]/(6.*Eb*I))*((6.*Le - 3.*x[0])*x[0] + (2. + nub)*pow(x[1], 2) - 3.*pow(D, 2)*0.5*(1.+nub))', '(coef_load*pow(x[1], 2)/(6.*Eb*I))*(3.*nub*pow(x[1],2)*(Le-x[0]) + (3.*Le - x[0])*pow(x[0], 2))'), coef_load = coef_load, Eb=Eb, I=I, Le=Le, nub=nub, D=D, degree = 3, domain=mesh)
 
-    u_exact = Expression(('20.*x[0]*pow(x[1], 3)', '5.*pow(x[0], 4)-5.*pow(x[1], 4)'), degree = 4)
+    p_exact = -Constant(kappa)*ufl.div(u_exact)
 
-    (u, p, u_l) = TrialFunctions(V)
-    (v, q, v_l) = TestFunctions(V)
+    (u, p) = TrialFunctions(V)
+    (v, q) = TestFunctions(V)
 
-    a = inner(grad(u), grad(v))*dx
-    b = - inner(p, div(v))*dx
-    c = - inner(div(u), q)*dx
+    a = 2.*mu*inner(sym(grad(u)), sym(grad(v)))*dx
+    b_1 = - inner(p, div(v))*dx
+    b_2 = - inner(q, div(u))*dx
+    c = (1./lmbda)*inner(p, q)*dx
 
-    L = FunctionSpace(mesh, 'R', 0)
-    u_l = TrialFunction(L)
-    v_l = TestFunction(L)
-
-    d = inner(u_l, q)*dx + inner(v_l, p)*dx
-    B = a + b + c + d
-
+    B = a + b_1 + b_2 - c
     L = inner(f, v)*dx
 
-    bcs = DirichletBC(V.sub(0), u_exact, 'on_boundary')
+    class LeftBoundary(SubDomain):
+        def inside(self, x, on_boundary):
+            return on_boundary and near(x[0], 0.)
+
+    class DirichletBoundary(SubDomain):
+        def inside(self, x, on_boundary):
+            return on_boundary and not near(x[0], 0.)
+
+    leftBoundary = LeftBoundary()
+    dirichletBoundary = DirichletBoundary()
+
+    bcs = DirichletBC(V.sub(0), Constant((0., 0.)), 'on_boundary')
 
     A, b = assemble_system(B, L, bcs=bcs)
 
@@ -150,31 +166,37 @@ def solve(V):
     with XDMFFile('output/pressure.xdmf') as f:
         f.write_checkpoint(p_h, 'p_h')
 
+    '''
+    u_exact_h = project(w_exact, V_u)
+    with XDMFFile('output/exact_displacement.xdmf') as xdmf:
+        xdmf.write_checkpoint(u_exact_h, 'u_exact_h')
+    '''
+
     X_el_f = VectorElement('CG', triangle, 3)
     M_el_f = FiniteElement('CG', triangle, 2)
 
-    V_el_f = MixedElement([X_el_f, M_el_f])
+    X_f = FunctionSpace(mesh, X_el_f)
+    M_f = FunctionSpace(mesh, M_el_f)
 
-    V_f = FunctionSpace(mesh, V_el_f)
+    u_h_f = project(u_h, X_f)
+    p_h_f = project(p_h, M_f)
 
-    w_h_f = project(w_h, V_f)
-    w_f = project(w_exact, V_f)
+    u_f = project(u_exact, X_f)
+    p_f = project(p_exact, M_f)
 
-    with XDMFFile('output/exact_disp.xdmf') as f:
-        f.write_checkpoint(w_f.sub(0), 'exact_disp')
+    u_diff = Function(X_f)
+    u_diff.vector()[:] = u_f.vector()[:] - u_h_f.vector()[:]
 
-    with XDMFFile('output/exact_p.xdmf') as f:
-        f.write_checkpoint(w_f.sub(1), 'exact_p')
+    p_diff = Function(M_f)
+    p_diff.vector()[:] = p_f.vector()[:] - p_h_f.vector()[:]
 
-    w_diff = Function(V_f)
-    w_diff.vector()[:] = w_h_f.vector()[:] - w_f.vector()[:]
-    local_exact_err_2 = energy_norm(w_diff)
+    local_exact_err_2 = energy_norm(u_diff, p_diff)
     exact_err = sqrt(sum(local_exact_err_2[:]))
     return w_h, exact_err
 
 
 def estimate(w_h):
-    """Estimator described in Section 3.3 of Liao and Silvester"""
+    """Estimator described in Section 3.5 of Khan et al."""
     mesh = w_h.function_space().mesh()
 
     u_h = w_h.sub(0)
@@ -196,30 +218,32 @@ def estimate(w_h):
     bcs = DirichletBC(X_f, Constant((0., 0.)), 'on_boundary', 'geometric')
 
     n = FacetNormal(mesh)
-    R_T = f + div(grad(u_h)) - grad(p_h)
+    R_K = f + div(2.*mu*sym(grad(u_h))) - grad(p_h)
     I = Identity(2)
 
-    R_E = (1./2.)*jump(-p_h*I + grad(u_h), -n)
+    R_E = (1./2.)*jump(p_h*I - 2.*mu*sym(grad(u_h)), -n)
+    rho_d = 1./(lmbda**(-1)+(2.*mu)**(-1))
 
-    a_X_e = inner(grad(e_X), grad(v_X))*dx
-    L_X_e = inner(R_T, v_X)*dx - inner(R_E, 2.*avg(v_X))*dS
+    a_X_e = 2.*mu*inner(grad(e_X), grad(v_X))*dx
+    L_X_e = inner(R_K, v_X)*dx - inner(R_E, avg(v_X))*dS
 
     e_h = fenics_error_estimation.estimate(a_X_e, L_X_e, N_X, bcs)
 
-    r_T = avg(div(u_h))
+    r_K = div(u_h) + (1./lmbda)*p_h
 
     V_e = FunctionSpace(mesh, 'DG', 0)
     v = TestFunction(V_e)
 
     eta_h = Function(V_e)
-    eta = assemble(inner(inner(grad(e_h), grad(e_h)), v)*dx + inner(inner(r_T, r_T), v)*dx)
+    eta = assemble(2.*mu*inner(inner(grad(e_h), grad(e_h)), v)*dx + \
+          rho_d*inner(inner(r_K, r_K), v)*dx)
     eta_h.vector()[:] = eta
 
     return eta_h
 
 
 def residual_estimate(w_h):
-    """Residual estimator described in Section 3.1 of Liao and Silvester"""
+    """Residual estimator described in Section 3.1 of Khan et al."""
     mesh = w_h.function_space().mesh()
 
     f = Constant((0., 0.))
@@ -228,10 +252,10 @@ def residual_estimate(w_h):
     p_h = w_h.sub(1)
 
     n = FacetNormal(mesh)
-    R_T = f + div(grad(u_h)) - grad(p_h)
-    r_T = div(u_h)
+    R_K = f + div(2.*mu*sym(grad(u_h))) - grad(p_h)
+    r_K = div(u_h) + (1./lmbda)*p_h
     I = Identity(2)
-    R_E = (1./2.)*jump(-p_h*I + grad(u_h), -n)
+    R_E = (1./2.)*jump(p_h*I - 2.*mu*sym(grad(u_h)), -n)
 
     V = FunctionSpace(mesh, "DG", 0)
     v = TestFunction(V)
@@ -239,25 +263,32 @@ def residual_estimate(w_h):
 
     eta_h = Function(V)
 
-    eta = assemble(h**2*R_T**2*v*dx + r_T**
-                   2*v*dx + avg(h)*R_E**2*avg(v)*dS)
+    rho_K = (h*(2.*mu)**(-0.5))/2.
+    rho_E = (avg(h)*(2.*mu)**(-1))/2.
+    rho_d = 1./(lmbda**(-1)+(2.*mu)**(-1))
+
+    eta = assemble(rho_K**2*R_K**2*v*dx + rho_d*r_K **
+                   2*v*dx + rho_E*R_E**2*avg(v)*dS)
     eta_h.vector()[:] = eta
 
     return eta_h
 
 
-def energy_norm(x):
-    mesh = x.function_space().mesh()
-    u = x.sub(0)
-    p = x.sub(1)
+def energy_norm(u, p):
+    u_mesh = u.function_space().mesh()
+    p_mesh = p.function_space().mesh()
+
+    assert u_mesh is p_mesh
+
+    mesh = u_mesh
 
     W = FunctionSpace(mesh, 'DG', 0)
     v = TestFunction(W)
-
-    form = inner(inner(grad(u), grad(u)), v)*dx + inner(inner(p, p), v)*dx
+    
+    form = Constant(2.*mu)*inner(inner(grad(u), grad(u)), v)*dx + Constant(1./(2.*mu)) * \
+        inner(inner(p, p), v)*dx + Constant(1./lmbda)*inner(inner(p, p), v)*dx
     norm_2 = assemble(form)
     return norm_2
-
 
 if __name__ == "__main__":
     main()
